4602f30241a273fe6c159c20fe27b4d87180b3b4
Mel <mel@Mels-MacBook-Pro.local>
2024-05-16 11:21:20 -0600

    fixing things

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -9,6 +9,15 @@ class Api::V0::VendorsController < ApplicationController
     status: :no_content
   end
 
+  def show
+    vendor = Vendor.find_by(id: params[:id])
+    if vendor
+      render json: VendorSerializer.new(vendor)
+    else
+      render json: { errors: [{ detail: "Can't find Vendor with 'id'=#{params[:id]}" }] }, status: :not_found
+    end
+  end
+
   private
 
   def vendor_params
--- spec/requests/api/v0/vendor_request_spec.rb
+++ spec/requests/api/v0/vendor_request_spec.rb
@@ -76,5 +76,45 @@ describe "Vendors API" do
       expect(data[:errors].first[:status]).to eq("400")
       expect(data[:errors].first[:title]).to eq("Validation failed: Description can't be blank, Contact name can't be blank")
     end
+
+    it "returns a 404 if an invalid id is given" do
+      get "/api/v0/vendors/123123132123"
+  
+      expect(response).to have_http_status(404)
+      error_response = JSON.parse(response.body, sympboliez_names: true)
+  
+      expect(error_response[:errors]).to be_a Array
+      expect(error_response[:errors].first[:detail]).to eq("Couldn't find Vendor with 'id'=123123132123")
+    end
+  end
+
+  #get one vendor
+  it "returns a single vendor info" do
+    id = create(:vendor).id
+    get "/api/v0/vendors/#{id}"
+
+    expect(response).to be_successful
+
+    vendor_data = JSON.parse(response.body, symbolize_names: true)
+
+    vendor = vendor_data[:data]
+
+      expect(vendor[:attributes]).to have_key(:id)
+      expect(vendor[:attributes][:id]).to be_a String
+
+      expect(vendor[:attributes]).to have_key(:name)
+      expect(vendor[:attributes][:name]).to be_a String
+
+      expect(vendor[:attributes]).to have_key(:description)
+      expect(vendor[:attributes][:description]).to be_a String
+
+      expect(vendor[:attributes]).to have_key(:contact_name)
+      expect(vendor[:attributes][:contact_name]).to be_a String
+
+      expect(vendor[:attributes]).to have_key(:contact_phone)
+      expect(vendor[:attributes][:contact_phone]).to be_a String
+
+      expect(vendor[:attributes]).to have_key(:credit_accepted)
+      expect(vendor[:attributes][:credit_accepted]).to be_in([true, false])
   end
 end
