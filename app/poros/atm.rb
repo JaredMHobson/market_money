f23c430e0bdd254c2baa37a058db897bc98312c3
Mel <mel@Mels-MacBook-Pro.local>
2024-05-16 18:27:15 -0600

    poro complete

--- app/poros/atm.rb
+++ app/poros/atm.rb
@@ -1,3 +1,16 @@
 class Atm
-  
+  attr_reader :lon,
+              :lat,
+              :dist,
+              :name,
+              :address
+
+  def initialize(attributes)
+    @lon = attributes[:lon]
+    @lat = attributes[:lat]
+    @dist = attributes[:dist]
+    @name = attributes[:name]
+    @address = attributes[:address]
+  end
+
 end
--- spec/facades/atm_facade_spec.rb
+++ spec/facades/atm_facade_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Atm Facade" do
-  
+  it "exists" do
+    market = create(:market)
+    facade = AtmFacade.new(market)
+
+    expect(facade).to be_an_instance_of AtmFacade
+  end
+
+  it "has a market attribute" do
+    market = create(:market)
+    facade = AtmFacade.new(market)
+
+    expect(facade.market).to be_an_instance_of Market
+  end
+
+  it "#nearby_atms" do
+    market = create(:market)
+    facade = AtmFacade.new(market)
+    atms = facade.nearby_atms
+
+    expect(atm_locations).to be_a Array
+    atms.each do |atm|
+      expect(atm[:lon]).to be_a Float
+      expect(atm[:lat]).to be_a Float
+      expect(atm[:dist]).to be_a Float
+      expect(atm[:name]).to be_a String
+      expect(atm[:address]).to be_a String
+    end
+  end
 end
