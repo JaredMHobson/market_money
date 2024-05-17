eb70fc6cdac6e4002b7774eb1ab1394c1533611f
Mel <mel@Mels-MacBook-Pro.local>
2024-05-16 19:52:48 -0600

    doing readme

--- spec/facades/atm_facade_spec.rb
+++ spec/facades/atm_facade_spec.rb
@@ -16,17 +16,18 @@ RSpec.describe "Atm Facade" do
   end
 
   it "#nearby_atms", :vcr do
-    market = create(:market)
+    market = Market.new({ lat: 36.98844,
+    lon: -121.97483, name: "Blah", street: "123 Main St", city: "Denver", county: "Denver", state: "CO", zip: "80303" })
     facade = AtmFacade.new(market)
     atms = facade.nearby_atms
 
     expect(atms).to be_a Array
     atms.each do |atm|
-      expect(atm[:lon]).to be_a Float
-      expect(atm[:lat]).to be_a Float
-      expect(atm[:distance]).to be_a Float
-      expect(atm[:name]).to be_a String
-      expect(atm[:address]).to be_a String
+      expect(atm.lon).to be_a Float
+      expect(atm.lat).to be_a Float
+      expect(atm.distance).to be_a Float
+      expect(atm.name).to be_a String
+      expect(atm.address).to be_a String
       expect(atm).to be_a Atm
     end
   end
